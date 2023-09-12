a8ac55eef2c85250a58fd2f0e2b38f5495c1c8b0
Tife <tife@Tifes-MBP.home>
2023-09-12 20:50:52 +0200

    feat: add count lines and count bytes flags

--- main.go
+++ main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin))
+	lines := flag.Bool("l", false, "count lines")
+	bytes := flag.Bool("b", false, "count bytes")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
-	scanner.Split(bufio.ScanWords)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
+
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
 
 	wc := 0
 	for scanner.Scan() {
--- main_test.go
+++ main_test.go
@@ -7,9 +7,31 @@ import (
 
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("one two three four\n")
-	got := count(b)
+	got := count(b, false, false)
 
 	exp := 4
+
+	if got != exp {
+		t.Errorf("expected %d, got %d", exp, got)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("one two three four\n")
+	got := count(b, true, false)
+
+	exp := 1
+
+	if got != exp {
+		t.Errorf("expected %d, got %d", exp, got)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("one two three four\n")
+	got := count(b, false, true)
+
+	exp := 19
 
 	if got != exp {
 		t.Errorf("expected %d, got %d", exp, got)
